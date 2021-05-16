52621bff7ef22eae23d628cc731da52a217390b9
Hasan Salim Kanmaz <h.kanmaz@faktion.com>
2021-05-16 21:26:01 +0200

    align d3 plot to the center

--- d3_project.js
+++ d3_project.js
@@ -22,7 +22,7 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickFormat(d3.format(".2s"));
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#d3plot").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
